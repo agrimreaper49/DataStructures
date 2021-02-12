1b9d2aad8f931993ca1be449c6d3d46eba599a14
Agrim Sharma <76flyershd@gmail.com>
2021-02-12 12:52:21 -0500

    added new projects that involve sets and maps

---
+++ FifaRunner/src/FifaRunner.java
@@ -0,0 +1,165 @@
+import java.io.*;
+import java.util.*;
+ 
+public class FifaRunner {
+ 
+    // Make static fields for each data structure you need.
+	
+	//Sets
+	private static TreeSet<Integer> ages = new TreeSet<>();
+	private static TreeSet<String> nationalities = new TreeSet<>();
+	private static HashSet<String> positions = new HashSet<>();
+	private static HashMap<String, Integer>  positionCount = new HashMap<>();
+	
+   
+  
+    /**
+     * Runs the program.
+     * @param args
+     */
+    public static void main(String[] args) {
+ 
+        // Change this location if you need to.
+        processFile("./src/fifa");
+        printResults();
+    }
+   
+   
+    /**
+     * Stores the information into the data structures.
+     * @param age The age of the player.
+     * @param position The position of the player.
+     * @param nationality The nationality of the player.
+     */
+    private static void processLine(int age, String position, String nationality) {
+       ages.add(age);
+       nationalities.add(nationality);
+       positions.add(position);
+//       positionCount.put(position, value)
+    }
+ 
+   
+    /**
+     * Prints the results of each exercise.
+     * Each print has already been set-up with basic titles,
+     * but you will need to add to the prints to get the correct results
+     * from your data structures.
+     */
+    private static void printResults() {
+        //A Determine the range of ages.
+        //  Print all of the ages.
+        //  What is the youngest age? What is the oldest age? (Use methods to find this)
+        System.out.println("All ages: " + ages.toString());
+        System.out.println("Youngest: " + ages.first());
+        System.out.println("Oldest: " + ages.last());
+        System.out.println();
+       
+       
+        //B Determine the number of different nationalities.
+        //  Print the nationalities in alphabetical order.
+        //  Print the number of unique nationalities.
+        System.out.println("All nationalities: " + nationalities.toString());
+        System.out.println("Unique nationalities: " + nationalities.size());
+        System.out.println();
+       
+        //C Determine the number of different positions.
+        //  Print all of the positions, order is not relevant.
+        //  Print the number of unique positions.
+        System.out.println("All positions: " + positions.toString());
+        System.out.println("Unique positions: " + positions.size());
+        System.out.println();
+       
+       
+        //D Wait for the lesson on Maps
+       
+       
+        //E Determine the number of players per position. IE: GK (goalkeeper) may have 85 players.
+        //  Print the results in any order.
+        System.out.println("Position count: ");
+        System.out.println();
+     
+        //F Determine the number of players per nation.
+        //  Print the results in any alphabetical order.
+        System.out.println("Nationality count: ");
+        System.out.println();
+       
+        //F Determine the oldest player per nationality.
+        //  Print the results in alphabetical order.
+        System.out.println("Oldest per nation: ");
+        System.out.println();
+       
+        //G Determine the number of players per age group.
+        //  We will define age groups by multiples of 5.
+        //  For instance, anyone aged 16, 17, 18, 19, 20 is in group 20.
+        //  Anyone aged 21, 22, 23, 24, 25 is in group 25.
+        //  Etcetera. Want the math? ---> scroll right                                                                                                                                                                                              5 * (int)Math.ceil(age / 5.0)
+        //  Print the results in numerical order by age (youngest group first).
+        System.out.println("Age group count: ");
+        System.out.println();
+       
+        //H Build a Map of Nationalities to Positions.
+        //  Print each nation on its own individual line followed by all of the positions that nation has.
+        //  Also print the nation which has the most unique number of positions and how many it has.
+        System.out.println("All positions in each nation:");
+       
+        System.out.println("Country with most positions: " + null + "(" + 0 + ")");
+    }
+   
+ 
+ 
+    /**
+     * This method is finished as is.
+     * Runs through the given file pulling out the age, position, and nationality.
+     * For each of the inputs, it will call the processLine() method.
+     * @param filename The location/name of the file to process.
+     */
+    private static void processFile(String filename) {        
+        Scanner scan = null;
+        try {
+            scan = new Scanner( new File( filename ) );
+            while ( scan.hasNext() ) {
+                int age = scan.nextInt();
+                String position = scan.next();
+                String nationality = scan.nextLine().trim();
+                processLine( age, position, nationality );
+            }            
+        }
+        catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        finally {
+            if ( scan != null ) {
+                scan.close();
+            }
+        }
+    }
+}
+ 
+ 
+/* Sample partial output
+ 
+All ages: [16, 17, ..]
+Youngest: _#_
+Oldest:   _#_
+ 
+All nationalities: [Afghanistan, Albania, ..]
+Unique nationalities: _#_
+ 
+All positions: [RS, LM, ..]
+Unique positions: _#_
+ 
+Position count: {RS=203, LM=1095, ..}
+ 
+Nationality count: {Afghanistan=4, Albania=39, ..}
+ 
+Oldest per nation: {Afghanistan=26, Albania=34, ..}
+ 
+Age group count: {20=3318, ..}
+ 
+All positions in each nation:
+Afghanistan:[CAM, CM, LB, LM]
+Albania:[CAM, CB, CDM, CM, GK, LB, LCM, LDM, LM, LW, RB, RCB, RCM, RM, RS, RW, ST]
+...
+Country with most positions: ______(_#_)
+ 
+*/
--- Sets/src/UniqueWords.java
+++ Sets/src/UniqueWords.java
@@ -1,24 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class UniqueWords {
 	public static void main (String [] args) {
-		final long START = System.currentTimeMillis();
-		// Do something
-		System.out.println(uniqueWordsArray("Aardvark")) ;
-		System.out.println(uniqueWordsHashSet("Aardvark")) ;
-		final long END = System.currentTimeMillis();
-		System.out.println("Total execution time: " + (END - START));
-
+		System.out.println(uniqueWordsArray("input"));
+		System.out.println(uniqueWordsHashSet("input"));
+		System.out.println(hashSetFile());
+		System.out.println(treeSetFile());
 	}
 	
 	public static int uniqueWordsArray(String input) {
 		boolean [] arr = new boolean [128];
 		for (int i = 0; i < input.length(); i++) {
-			arr[input.charAt(i)]  = true;
+			arr[input.charAt(i)] = true;
 		}
 		int counter = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == true) {
+			if (arr[i]) {
 				counter++;
 			}
 		}
@@ -26,11 +25,37 @@ public class UniqueWords {
 	}
 	
 	public static int uniqueWordsHashSet(String input) {
-		Set<String> unique = new HashSet<>();
+		Set<Character> unique = new HashSet<>();
 		for (int i = 0; i < input.length(); i++) {
-			String s = input.charAt(i) + "";
-			unique.add(s);
+			char c = input.charAt(i);
+			unique.add(c);
 		}
 		return unique.size();
+	}
+	
+	public static int hashSetFile() {
+		Set<String> uniqueHash = new HashSet<>();
+		try {
+			Scanner fileReader = new Scanner(new File("./src/harryPotter"));
+			while (fileReader.hasNext()) {
+			    uniqueHash.add(fileReader.next());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return uniqueHash.size();
+	}
+	
+	public static int treeSetFile() {
+		Set<String> uniqueTree = new TreeSet<>();
+		try {
+			Scanner fileReader = new Scanner(new File("./src/harryPotter"));
+			while (fileReader.hasNext()) {
+			    uniqueTree.add(fileReader.next());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return uniqueTree.size();
 	}
 }
